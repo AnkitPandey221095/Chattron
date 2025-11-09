ff74629ee795de82d497e1e5a71782bf71ad72b6
AnkitPandey221095 <ankitpandey221095@gmail.com>
2025-11-09 14:30:39 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import path from 'path'
 
 //environment variable configuration
 dotenv.config()
@@ -11,11 +12,18 @@ const PORT = process.env.PORT || 3000
 // Starting web server
 const app= express()
 
-
-
-
 //Creating routes
 app.use('/api/auth',authRoutes)
+
+const __dirname= path.resolve()
+
+//making for production ready
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../frontend/dist/index.html'))
+    })
+}
 
 // listing server
 app.listen(PORT, ()=>{
