f9be146ff6504175e1a0d0b9673dfd592a15d13c
AnkitPandey221095 <ankitpandey221095@gmail.com>
2025-11-28 23:06:03 +0530

    Added login logout api feature

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -72,3 +72,32 @@ export const signup = async (req, res)=>{
         res.status(500).json({message:"Server error"})
     }
 }
+
+export const login = async (req, res)=>{
+    const {email, password} = req.body
+
+    try{
+        const user = await User.findOne({email})
+        if (!user) return res.status(400).json({message:"Invalide email or password"})
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if(!isPasswordCorrect) return res.status(200).json({message:"Invalide useId or password"})
+
+        generateToken(user._id,res)
+
+        res.status(200).json({
+            _id:user._id,
+            email: user.email,
+            fullname: user.fullname,
+            profilePic: user.profilePic
+        })
+
+    }catch(err){
+        console.error("Error in the login controller: ", erroe)
+    }
+}
+
+export const logout = async (_, res)=>{
+    res.cookie('jwt',"", {maxAge:0})
+    res.status(200).json({message:"logout successfully"})
+}
